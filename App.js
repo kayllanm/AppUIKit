3fe89a800a74243cabdb528c62ae1162e62e23b3
Kayllan Morris <kayllan.morris@IWIJHBANUHTD8.local>
2020-07-21 14:07:32 +0200

    added button

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+          onPress={() => alert('Hello, world!')}
+          style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +23,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonStyle: {
+    width: 100,
+    height:20,
+    backgroundColor: 'red'
+  }
 });
